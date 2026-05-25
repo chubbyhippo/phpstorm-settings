430add4dbaa499721f919804f53f3ad440fddf81
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-05-25 18:42:21 +0700

    Create install-plugins.sh

---
+++ install-plugins.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# plugins
+cmd=phpstorm
+if uname | grep -q "^MINGW"; then
+  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/phpstorm.cmd
+fi
+
+$cmd installPlugins \
+IdeaVIM \
+com.github.camork.fileExpander \
+com.joshestein.ideavim-quickscope \
+com.julienphalip.ideavim.functiontextobj \
+com.julienphalip.ideavim.peekaboo \
+eu.theblob42.idea.whichkey
